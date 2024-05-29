033a6cd150981d8afa7e647679cc75f54bc0ad59
Kyunghyun Cho <kc119@log-1.hpc.nyu.edu>
2024-05-29 08:51:20 -0400

    fixed the read method

--- array_io.py
+++ array_io.py
@@ -24,12 +24,10 @@ def read_pair_from_file(file):
     id_bytes = file.read(id_length)
     id_val = id_bytes.decode('utf-8')
 
-    # Read int64
-    id_val = struct.unpack('q', file.read(8))[0]
     # Read the shape of the numpy array
     rows = struct.unpack('i', file.read(4))[0]
     # Read the numpy array data
-    np_array = np.frombuffer(file.read(rows * 2), dtype=np.float64).reshape(rows)
+    np_array = np.frombuffer(file.read(rows * 2), dtype=np.float16).reshape(rows)
 
     return id_val, np_array
 
